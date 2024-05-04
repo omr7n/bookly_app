251518c6e72173cb817f722ed172353765223071
omran abo ali <omranza12@gmail.com>
2024-05-04 17:54:46 +0300

    create search View

---
+++ lib/features/search/presentation/views/search_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/search_view_body.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: SearchViewBody()));
+  }
+}
+
