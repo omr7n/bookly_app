6927bedbae573894d332e0297a3f7be2bc31c414
omran abo ali <omranza12@gmail.com>
2024-05-04 18:41:23 +0300

    create custom search text filed

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,42 @@
+import 'package:bookly_app/features/search/presentation/views/widgets/custom_search_text_filed.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        children: [
+          CustomSearchTextFiled(hintText: "", icons: 
+          FontAwesomeIcons.magnifyingGlass,
+        
+        )
+        ],
+      ),
+    );
   }
 }
+
+// class CustomSearchTextFiled extends StatelessWidget {
+//   const CustomSearchTextFiled({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const TextField(
+//       decoration: InputDecoration(
+//         enabledBorder: OutlineInputBorder(),
+//         hintText: "Search",
+//         suffixIcon: Icon(
+//           FontAwesomeIcons.magnifyingGlass,
+//           size: 22,
+//         ),
+//       ),
+//     );
+//   }
+// }
