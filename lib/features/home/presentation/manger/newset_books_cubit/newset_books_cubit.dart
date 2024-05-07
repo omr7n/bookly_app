82e8921c7714ad89b6bb36040bb11a971cabb73f
omran abo ali <omranza12@gmail.com>
2024-05-07 17:01:03 +0300

    newset books  cubit done

--- lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
-import 'package:meta/meta.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewSetBooks();
+    result.fold((failure) {
+      return NewsetBooksFailure(errorMessage: failure.toString());
+    }, (books) {
+      return NewsetBooksSuccess(books: books);
+    });
+  }
 }
